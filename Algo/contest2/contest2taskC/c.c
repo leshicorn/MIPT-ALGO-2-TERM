a0ec63368a45083747798e620412a904c9c11a17
leshicorn <romanenkov2003@mail.ru>
2023-04-18 20:36:14 +0300

    c task fixed

--- Algo/contest2/contest2taskC/c.c
+++ Algo/contest2/contest2taskC/c.c
@@ -6,26 +6,51 @@
 
 #include <string.h>
 
-
-typedef struct Node{
+/**
+ * @brief Node of the tree
+ *
+ */
+typedef struct Node
+{
     int key;
     int priority;
     int size;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 } Node;
 
-static inline int size(Node* root) {
+/**
+ * @brief Get the size of the tree
+ *
+ * @param root
+ * @return int
+ */
+static inline int size(Node *root)
+{
     return root ? root->size : 0;
 }
 
-static inline void update_size(Node* root) {
-    if (root) {
+/**
+ * @brief Update the size of the tree
+ *
+ * @param root
+ */
+static inline void update_size(Node *root)
+{
+    if (root)
+    {
         root->size = size(root->left) + size(root->right) + 1;
     }
 }
 
-Node* createNode(int key){
+/**
+ * @brief Create a Node object
+ *
+ * @param key
+ * @return Node*
+ */
+Node *createNode(const int key)
+{
     Node *node = malloc(sizeof(Node));
     node->key = key;
     node->priority = rand();
@@ -35,37 +60,67 @@ Node* createNode(int key){
     return node;
 }
 
-Node* merge(Node* left, Node* right) {
-    if (!left || !right) return left ? left : right;
+/**
+ * @brief Merge two trees
+ *
+ * @param left
+ * @param right
+ * @return Node*
+ */
+Node *merge(Node *left, Node *right)
+{
+    if (!left || !right)
+        return left ? left : right;
 
-    if (left->priority > right->priority) {
+    if (left->priority > right->priority)
+    {
         left->right = merge(left->right, right);
         return left;
     }
-    else {
+    else
+    {
         right->left = merge(left, right->left);
         return right;
     }
 }
 
-void split(Node *node, int key, Node **left, Node **right) {
-    if (!node) {
+/**
+ * @brief Split the tree into two trees
+ *
+ * @param node
+ * @param key
+ * @param left
+ * @param right
+ */
+void split(Node *node, const int key, Node **left, Node **right)
+{
+    if (!node)
+    {
         *left = NULL;
         *right = NULL;
         return;
     }
-    if (node->key < key) {
+    if (node->key < key)
+    {
         *left = node;
         split(node->right, key, &((*left)->right), right);
     }
-    else {
+    else
+    {
         *right = node;
         split(node->left, key, left, &((*right)->left));
     }
 }
 
-Node* rotate_right(Node* node) {
-    Node* new_root = node->left;
+/**
+ * @brief Rotates tree right side by given
+ *
+ * @param node
+ * @return Node*
+ */
+Node *rotate_right(Node *node)
+{
+    Node *new_root = node->left;
     node->left = new_root->right;
     new_root->right = node;
     update_size(node);
@@ -73,8 +128,15 @@ Node* rotate_right(Node* node) {
     return new_root;
 }
 
-Node* rotate_left(Node* node) {
-    Node* new_root = node->right;
+/**
+ * @brief Rotates tree left side by given
+ *
+ * @param node
+ * @return Node*
+ */
+Node *rotate_left(Node *node)
+{
+    Node *new_root = node->right;
     node->right = new_root->left;
     new_root->left = node;
     update_size(node);
@@ -82,9 +144,19 @@ Node* rotate_left(Node* node) {
     return new_root;
 }
 
-Node *insert(Node *root, int key, int priority) {
-    if (root == NULL) {
-        Node* new_root = (Node*) malloc(sizeof(Node));
+/**
+ * @brief Insert a new node into the tree
+ *
+ * @param root
+ * @param key
+ * @param priority
+ * @return Node*
+ */
+Node *insert(Node *root, const int key, const int priority)
+{
+    if (root == NULL)
+    {
+        Node *new_root = (Node *)malloc(sizeof(Node));
         new_root->key = key;
         new_root->priority = priority;
         new_root->size = 1;
@@ -92,14 +164,19 @@ Node *insert(Node *root, int key, int priority) {
         return new_root;
     }
 
-    if (key < root->key) {
+    if (key < root->key)
+    {
         root->left = insert(root->left, key, priority);
-        if (root->left->priority > root->priority) {
+        if (root->left->priority > root->priority)
+        {
             root = rotate_right(root);
         }
-    } else if (key > root->key) {
+    }
+    else if (key > root->key)
+    {
         root->right = insert(root->right, key, priority);
-        if (root->right->priority > root->priority) {
+        if (root->right->priority > root->priority)
+        {
             root = rotate_left(root);
         }
     }
@@ -108,39 +185,63 @@ Node *insert(Node *root, int key, int priority) {
     return root;
 }
 
-Node* delete(Node* root, int key) {
-    if (!root) {
+/**
+ * @brief Delete a node from the tree
+ *
+ * @param root
+ * @param key
+ * @return Node*
+ */
+Node *delete(Node *root, const int key)
+{
+    if (!root)
+    {
         return root;
     }
-    if (key < root->key) {
-        root->left = delete(root->left, key);
-    } else if (key > root->key) {
-        root->right = delete(root->right, key);
-    } else {
-        if (!root->left || !root->right) {
-            Node* new_root = root->left ? root->left : root->right;
+    if (key < root->key)
+    {
+        root->left = delete (root->left, key);
+    }
+    else if (key > root->key)
+    {
+        root->right = delete (root->right, key);
+    }
+    else
+    {
+        if (!root->left || !root->right)
+        {
+            Node *new_root = root->left ? root->left : root->right;
             free(root);
             root = new_root;
-        } else {
-            Node* node = root->left;
-            while (node->right) {
+        }
+        else
+        {
+            Node *node = root->left;
+            while (node->right)
+            {
                 node = node->right;
             }
             root->key = node->key;
-            root->left = delete(root->left, node->key);
+            root->left = delete (root->left, node->key);
         }
     }
-    if (root) {
-        if (root->left) {
-            if (root->left->priority > root->priority) {
-                Node* new_root = root->left;
+    if (root)
+    {
+        if (root->left)
+        {
+            if (root->left->priority > root->priority)
+            {
+                Node *new_root = root->left;
                 root->left = new_root->right;
                 new_root->right = root;
                 root = new_root;
             }
-        } else if (root->right) {
-            if (root->right->priority > root->priority) {
-                Node* new_root = root->right;
+        }
+        else if (root->right)
+        {
+            if (root->right->priority > root->priority)
+            {
+                Node *new_root = root->right;
                 root->right = new_root->left;
                 new_root->left = root;
                 root = new_root;
@@ -151,115 +252,252 @@ Node* delete(Node* root, int key) {
     return root;
 }
 
-Node* search(Node* root, int key) {
-    while (root) {
-        if (key == root->key) {
+/**
+ * @brief Search for a node in the tree
+ *
+ * @param root
+ * @param key
+ * @return Node*
+ */
+Node *search(Node *root, const int key)
+{
+    while (root)
+    {
+        if (key == root->key)
+        {
             return root;
-        } else if (key < root->key) {
+        }
+        else if (key < root->key)
+        {
             root = root->left;
-        } else {
+        }
+        else
+        {
             root = root->right;
         }
     }
     return NULL;
 }
 
-bool exists(Node* root, int key) {
-    while (root) {
-        if (key == root->key) {
+/**
+ * @brief Check if a node exists in the tree
+ *
+ * @param root
+ * @param key
+ * @return true
+ * @return false
+ */
+bool exists(Node *root, const int key)
+{
+    while (root)
+    {
+        if (key == root->key)
+        {
             return true;
-        } else if (key < root->key) {
+        }
+        else if (key < root->key)
+        {
             root = root->left;
-        } else {
+        }
+        else
+        {
             root = root->right;
         }
     }
     return false;
 }
 
-Node* next(Node* root, int key) { 
-    Node* node = NULL;
-    while (root) {
-        if (key < root->key) {
+/**
+ * @brief Get the next node in the tree
+ *
+ * @param root
+ * @param key
+ * @return Node*
+ */
+Node *next(Node *root, const int key)
+{
+    Node *node = NULL;
+    while (root)
+    {
+        if (key < root->key)
+        {
             node = root;
             root = root->left;
-        } else if (key > root->key) {
+        }
+        else if (key > root->key)
+        {
             root = root->right;
-        } else {
+        }
+        else
+        {
             return root;
         }
     }
     return node;
 }
 
-Node* prev (Node* root, int key) {
-    Node* node = NULL;
-    while (root) {
-        if (key < root->key) {
+/**
+ * @brief Get the previous node in the tree
+ * 
+ * @param root 
+ * @param key 
+ * @return Node* 
+ */
+Node *prev(Node *root, const int key)
+{
+    Node *node = NULL;
+    while (root)
+    {
+        if (key < root->key)
+        {
             root = root->left;
-        } else if (key > root->key) {
+        }
+        else if (key > root->key)
+        {
             node = root;
             root = root->right;
-        } else {
+        }
+        else
+        {
             return root;
         }
     }
     return node;
 }
 
-int kth(Node* root, int k) {
-    if (k < 0 || k >= size(root) || !root) {
+/**
+ * @brief Get the kth node in the tree
+ * 
+ * @param root 
+ * @param k 
+ * @return int 
+ */
+int kth(Node *root, const int k)
+{
+    if (k < 0 || k >= size(root) || !root)
+    {
         return -1;
     }
     int s = size(root->left);
-    if (k == s) {
+    if (k == s)
+    {
         return root->key;
     }
-    if (k < s) {
+    if (k < s)
+    {
         return kth(root->left, k);
     }
     return kth(root->right, k - s - 1);
 }
 
+/**
+ * @brief Find the case of the operation
+ * 
+ * @param op 
+ * @return int 
+ */
+int FindCase(char *op)
+{
+    if (strcmp(op, "insert") == 0)
+    {
+        return 1;
+    }
+    else if (strcmp(op, "delete") == 0)
+    {
+        return 2;
+    }
+    else if (strcmp(op, "exists") == 0)
+    {
+        return 3;
+    }
+    else if (strcmp(op, "next") == 0)
+    {
+        return 4;
+    }
+    else if (strcmp(op, "prev") == 0)
+    {
+        return 5;
+    }
+    else if (strcmp(op, "kth") == 0)
+    {
+        return 6;
+    }
+    return 0;
+}
 
-int main() {
-    Node* root = NULL;
-    char* op = malloc(25 * sizeof(char));
+/**
+ * @brief Main function
+ * 
+ */
+void Solution()
+{
+    Node *root = NULL;
+    char *op = malloc(25 * sizeof(char));
     int operand_number;
-    while (scanf("%s %d", op, &operand_number) != EOF) {
-        if (strcmp(op, "insert") == 0) {    
-            if (!exists(root, operand_number)) {
+    int CaseNumber = -1;
+
+    while (scanf("%s %d", op, &operand_number) != EOF)
+    {
+        switch (CaseNumber = FindCase(op))
+        {
+        case 1:
+            if (!exists(root, operand_number))
+            {
                 int priority = rand();
                 root = insert(root, operand_number, priority);
             }
-        } else if (strcmp(op, "delete") == 0) {
-            if (exists(root, operand_number)) {
-                root = delete(root, operand_number);
+            break;
+        case 2:
+            if (exists(root, operand_number))
+            {
+                root = delete (root, operand_number);
             }
-        } else if (strcmp(op, "exists") == 0) {
+            break;
+        case 3:
             printf("%s\n", exists(root, operand_number) ? "true" : "false");
-        } else if (strcmp(op, "next") == 0) {
-            Node* p = next(root, operand_number);
-            if (p) {
+            break;
+        case 4:
+            Node *p = next(root, operand_number);
+            if (p)
+            {
                 printf("%d\n", p->key);
-            } else {
+            }
+            else
+            {
                 printf("none\n");
             }
-        } else if (strcmp(op, "prev") == 0) {
-            Node* p = prev(root, operand_number);
-            if (p) {
-                printf("%d\n", p->key);
-            } else {
+            break;
+        case 5:
+            Node *pt = prev(root, operand_number);
+            if (pt)
+            {
+                printf("%d\n", pt->key);
+            }
+            else
+            {
                 printf("none\n");
             }
-        } else if (strcmp(op, "kth") == 0) {
+            break;
+        case 6:
             int kth_value = kth(root, operand_number);
-            if (kth_value >= 0) {
+            if (kth_value >= 0)
+            {
                 printf("%d\n", kth_value);
-            } else {
+            }
+            else
+            {
                 printf("none\n");
             }
+            break;
+        default:
+            break;
         }
     }
+}
+
+int main()
+{
+
+    Solution();
     return 0;
 }
